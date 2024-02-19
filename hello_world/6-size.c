fcff1f81e49ce47d4dc9b7995d48406ddf51c4ef
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-19 13:42:27 +0000

    salam

---
+++ hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of char: %zu byte(s)\n", sizeof(char));
+    printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of long: %zu byte(s)\n", sizeof(long));
+    printf("Size of long long: %zu byte(s)\n", sizeof(long double));
+    printf("Size of float: %zu byte(s)\n", sizeof(float));
+    return 0;
+}

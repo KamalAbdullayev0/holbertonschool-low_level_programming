c980cd0ebb1303f9c46b00fb6cfa01df0aa67c0d
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-20 14:47:54 +0000

    a

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (l_d > 5)
+	if ( l_d > 5 )
 	{
 		printf("Last digit of %d is %d and is geater than 5\n", n, l_d);
 	}
-	else if (l_d == 0)
+	else if ( l_d == 0 )
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l_d);
 	}

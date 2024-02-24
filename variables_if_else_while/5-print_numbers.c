ac0b39d9887ab80ee56a59ce34ef05f22075197f
root <KamalAbdullayev0>
2024-02-24 16:18:03 +0000

    a

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char  ulvi = '0';
+
+	while (ulvi < 10)
+	{
+		putchar(ulvi);
+		ulvi++;
+	}
+
+	putchar('\n');
+	return (0);
+
+
+}

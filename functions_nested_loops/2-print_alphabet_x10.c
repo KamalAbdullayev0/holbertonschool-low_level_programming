499bf42e0164b5d972ae92b617c2523b061389cc
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-25 14:22:27 +0000

    a

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - prints alphabet 10 times
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char kamal = 'a';
 	int a = 0;

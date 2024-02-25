4c325ccf60cae356c181d39d85617e8c6b46bb94
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-25 16:39:37 +0000

    a

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: variable
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if(c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (65 >= c && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

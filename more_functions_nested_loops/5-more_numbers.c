71f7436aa3721a3429c3fe65526aff77102ff0c0
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-26 14:42:12 +0000

    a

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * more_numbers - A function that prints 10 times the numbers 0 to 14
- * @a: Variable
- * @b: Variable
- * Return: function value
+ * more_numbers - checks
+ *
+ * Return: 1 if upper, otherwise 0
  */
 
 void more_numbers(void)

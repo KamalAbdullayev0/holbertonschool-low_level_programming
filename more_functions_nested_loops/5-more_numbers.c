c5b592358851cdb3c7b4fb39e1452be94a30863c
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-26 14:06:09 +0000

    a

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,21 +9,13 @@ void more_numbers(void)
 {
 	int a = 0;
 	int b = 0;
-	while (a < 15)
+	while (a < 10)
 	{
-		b = a * 10;
-		if (b > 100)
-                {
-                        _putchar(1 + '0');
-		}
-		if (b > 10)
+		while(b < 15)
 		{
-			_putchar(b / 10 + '0')
+			_putchar(b + '0');
 		}
-		if (b < 10)
-                {
-                        _putchar(b + '0');
-                }
+	b = 0;
 	a++;
 	}
 }

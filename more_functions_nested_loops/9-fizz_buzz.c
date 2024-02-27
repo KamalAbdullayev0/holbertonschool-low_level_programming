f29a61342787fe157aba8d8c45418044ec817c2d
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-27 07:24:15 +0000

    a

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i = 1;
 
-	while (i < 100)
+	while (i <= 100)
 	{
 
-		if (i % 3 != 0 || i % 5 != 0)
+		if (i % 3 != 0 && i % 5 != 0)
 		{
 			printf("%d", i);
 		}

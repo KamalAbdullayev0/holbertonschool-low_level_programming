2543f6f988a1924fee8ce19e1ddfaa91d5be1b30
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-27 11:05:43 +0000

    a

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,18 +7,21 @@
 
 void print_triangle(int size)
 {
+	int i;
+	int j;
+
 	for (i = 1; i < size ; i++)
 	{
 
 		for (j = 0; j <= (size - (i + 2)); j++)
 		{
-			_putcha(' ');
+			_putchar(' ');
 		}
 		for (j = 0; j <= i; j++)
                 {
                         _putchar(23);
                 }
-		_putchar('\n')
+		_putchar('\n');
 
 	}
 	if (size <= 0)

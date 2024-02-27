4fca04ee52a6d94234d08e2b261b2b04f89c0a4a
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-27 07:08:20 +0000

    a

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizz-buzz - program that prints the numbers from 1 to 100
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while(i < 100)
+	{
+
+		if (i % 3 != 0 || i % 5 != 0)
+		{
+			printf("%d ",i);
+		}
+		else if (i % 3 == 0 || i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 3 != 0 || i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("Fizz Buzz ");
+		}
+	}
+}

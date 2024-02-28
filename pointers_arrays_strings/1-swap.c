a91464b949a9a6462ec6f5a36fbdaf4d278cae83
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-02-28 08:24:22 +0000

    rahim

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,7 +2,6 @@
 
 /**
  * swap_int - function that swaps the values of two integers
- * @tas: Variable
  * @a: pointer to the first integer
  * @b: pointer to the second integer
  * Return Always 0 (Success)

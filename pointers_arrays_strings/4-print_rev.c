80b8e92867f7135c6ad5bdbd6fc3afebdfb5d1be
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-03 10:11:33 +0000

    a

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s+i) != 0)
+	while (*(s + i) != 0)
 	{
 		i++;
 	}
 
 	while (i >= 0)
 	{
-		_putchar(*(s+i));
+		_putchar(*(s + i));
 		i--;
 	}
 	_putchar('\n');

0adbe0754c4cb1dd423f0eb3ed00d872598c5a6a
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-03 15:04:29 +0000

    a

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints n elements of an array
+ *
+ * @a: pointer
+ * @n: variable
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	_putchar("\n");
+}

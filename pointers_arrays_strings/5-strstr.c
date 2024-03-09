8048289e467cf2a159924d0f29c17c6afb952755
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-09 23:48:27 +0000

    a

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,18 +1,14 @@
-#include "main.h"
-#include <stdio.h>
 #include <string.h>
 #include <stddef.h>
 /**
- * *_strstr - check the code
- * @haystack: longer string
+ * _strstr - substring
+ * @haystack: main string
  * @needle: substring
- * Return: Always 0.
+ * Return: substring
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	int counter = 0;
-	int len = strlen(needle);
+	int i = 0, counter = 0, len = strlen(needle);
 
 	while (*(haystack) != '\0')
 	{

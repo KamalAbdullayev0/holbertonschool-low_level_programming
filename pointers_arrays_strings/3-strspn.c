3d0ab0145fafe7ecd8de12934824fb3dc82cc5b3
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-09 21:46:10 +0000

    a

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - does smth strange
+ * @s: string
+ * @accept: string2
+ *
+ * Return: un
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (*(s + i))
+	{
+		j = 0;
+		while (*(accept + j))
+		{
+			if (*(s + i) != *(accept + j))
+			{
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (k);
+}

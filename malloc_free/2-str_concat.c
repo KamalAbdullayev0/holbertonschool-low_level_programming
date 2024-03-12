da69a074a5b6acb02fceba7892f6c52b87e3701f
KamalAbdullayev <abdullayevkamal16.08@gmail.com>
2024-03-12 06:47:59 +0000

    d

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	if (s1 == NULL)
+		i = 0;
+	else
+		while *(s1 + i)
+			i++;
+	if (s2 == NULL)
+		while *(s2 + j)
+			j++;
+	str = malloc(sizeof(char) * i * j + 1);
+	if (str == NULL)
+		return (NULL);
+
+	while (k < i)
+	{
+		*(str + k) = *(s1 + k);
+		k++;
+	}
+	while (k < i + j)
+	{
+		*(str + k) = *(s2 + k - i);
+		k++;
+	}
+	str[k] = 0;
+	return (str);
+}

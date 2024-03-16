1155d6e624c51a57b60f1546e29e15a6484b4f43
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-16 16:37:34 +0000

    f

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _calloc - function
+ * @nmemb: number of elements
+ * @size: size of single byte
+ * Return: memory allocated pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *str;
+	unsigned int i = 0;
+
+	if (nmemb == NULL || size == NULL)
+		return (NULL);
+	str = malloc(nmemb * size);
+	if (str == NULL)
+		return (NULL);
+	while (i < size * nmemb)
+	{
+		*(str + i) = 0;
+		i++;
+	}
+	return (str);
+}

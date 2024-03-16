1374da68b1c20499a62f973c6cb3ac5584c20bb6
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-16 16:14:43 +0000

    d

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0;
 	char *str;
-	
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (*(s1 + i))
 	{
 		i++;

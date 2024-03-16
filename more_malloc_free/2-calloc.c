cdb6a102d32b50d96caf84fe400d8f59feae85a8
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-16 16:40:30 +0000

    g

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *str;
 	unsigned int i = 0;
 
-	if (nmemb == NULL || size == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	str = malloc(nmemb * size);
 	if (str == NULL)

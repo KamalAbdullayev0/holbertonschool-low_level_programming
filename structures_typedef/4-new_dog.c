e27dd7aadee71a15ffacb6c1acdfa734bc78a6dc
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-18 12:26:53 +0000

    f

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,27 +12,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	dog = malloc(sizeof(dog_t));
 
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	(*dog).name  = malloc(strlen(name) + 1);
-	if ((*dog).name == NULL)
+	dog->name = malloc(strlen(name) + 1);
+	if (dog->name == NULL)
 	{
-		free((*dog).name);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	(*dog).age = age;
-	(*dog).owner = malloc(strlen(owner) + 1);
-	if ((*dog).owner == NULL)
+	dog->age = age;
+	dog->owner = malloc(strlen(owner) + 1);
+	if (dog->owner == NULL)
 	{
-		free((*dog).owner);
-		free((*dog).name);
+		free(dog->owner);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	strcpy((*dog).name, name);
-	strcpy((*dog).owner, owner);
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
 	return (dog);
 }
